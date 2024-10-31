0f9dd0c0673dc8e038b414102cf4cf7e222f89bf
KyuWoo Choi <kyuwoo.choi@gmail.com>
2024-10-31 14:38:13 +0900

    feat: register nubison model to mlflow (#1)
    * test: tox error due to deps missing
    
    * feat: implement model register to mlflow
    
    * feat: reflect current env to mmodel env to register
    
    * fix: set experiment name instead of run name
    
    * feat: register model with code_paths
    
    * feat: better artifact handling model
    
    * feat: conda channel config is not necessary
    
    * refactor: srp make env in it's own func
    
    * chore: remove mlserver from dependency

---
+++ nubison_model/Model.py
@@ -0,0 +1,103 @@
+import mlflow
+
+from os import getenv
+from sys import version_info as py_version_info
+from typing import Optional, Protocol, runtime_checkable
+from importlib.metadata import distributions
+from mlflow.pyfunc import PythonModel
+
+DEFAULT_MODEL_NAME = "nubison_model"
+DEAFULT_MLFLOW_URI = "http://127.0.0.1:5000"
+DEFAULT_ARTIFACT_DIRS = ""  # Default code paths comma-separated
+
+
+@runtime_checkable
+class Model(Protocol):
+    def load_model(self) -> None: ...
+
+    def infer(self, input: any) -> any: ...
+
+
+def _make_conda_env() -> dict:
+    # Get the Python version
+    python_version = (
+        f"{py_version_info.major}.{py_version_info.minor}.{py_version_info.micro}"
+    )
+    # Get the list of installed packages
+    packages_list = sorted(
+        [
+            f"{dist.metadata['Name']}=={dist.version}"
+            for dist in distributions()
+            if dist.metadata["Name"]
+            is not None  # editable installs have a None metadata name
+        ]
+    )
+
+    return {
+        "dependencies": [
+            f"python={python_version}",
+            "pip",
+            {"pip": packages_list},
+        ],
+    }
+
+
+def _make_mlflow_model(nubison_model: Model) -> PythonModel:
+
+    class MLflowModel(PythonModel):
+        def load_context(self, context):
+            from os import path, symlink
+
+            for name, target_path in context.artifacts.items():
+                # Create the symbolic link with the key as the symlink name
+                try:
+                    symlink(
+                        target_path, name, target_is_directory=path.isdir(target_path)
+                    )
+                    print(f"Created symlink: {name} -> {target_path}")
+                except OSError as e:
+                    print(f"Error creating symlink for {name}: {e}")
+
+            nubison_model.load_model()
+
+        def predict(self, context, model_input):
+            return nubison_model.infer(model_input)
+
+    return MLflowModel()
+
+
+def register(
+    model: Model,
+    model_name: Optional[str] = None,
+    mlflow_uri: Optional[str] = None,
+    artifact_dirs: Optional[str] = None,
+):
+    # Check if the model implements the Model protocol
+    if not isinstance(model, Model):
+        raise TypeError("The model must implement the Model protocol")
+
+    # Get the model name and MLflow URI from environment variables if not provided
+    if model_name is None:
+        model_name = getenv("MODEL_NAME", DEFAULT_MODEL_NAME)
+    if mlflow_uri is None:
+        mlflow_uri = getenv("MLFLOW_TRACKING_URI", DEAFULT_MLFLOW_URI)
+    # Get the list of artifact directories.
+    # If not provided, read from environment variables, else use the default
+    artifact_dirs = artifact_dirs or getenv("ARTIFACT_DIRS", DEFAULT_ARTIFACT_DIRS)
+    artifact_dirs = {
+        dir.strip(): dir.strip() for dir in artifact_dirs.split(",") if dir != ""
+    }
+
+    mlflow.set_tracking_uri(mlflow_uri)
+    mlflow.set_experiment(model_name)
+
+    # Start a new MLflow run
+    with mlflow.start_run() as run:
+        # Log the model to MLflow
+        mlflow.pyfunc.log_model(
+            artifact_path="",
+            python_model=_make_mlflow_model(model),
+            conda_env=_make_conda_env(),
+            registered_model_name=model_name,
+            artifacts=artifact_dirs,
+        )
---
+++ test/test_model.py
@@ -0,0 +1,125 @@
+import pytest
+
+from os import environ, path, getcwd, makedirs, chdir
+from contextlib import contextmanager
+from typing import List
+from shutil import rmtree
+
+from mlflow.tracking import MlflowClient
+from mlflow.pyfunc import load_model
+from nubison_model.Model import Model, register
+
+
+@contextmanager
+def temporary_dirs(dirs: List[str]):
+    dirs = [path.join(getcwd(), src_dir) for src_dir in dirs]
+
+    try:
+        for dir in dirs:
+            makedirs(dir, exist_ok=True)
+
+        yield dirs
+
+    finally:
+        for dir in dirs:
+            if path.exists(dir):
+                rmtree(dir)
+
+
+@contextmanager
+def temporary_cwd(new_dir):
+    original_dir = getcwd()
+    try:
+        chdir(new_dir)
+        yield
+    finally:
+        chdir(original_dir)
+
+
+@contextmanager
+def temporary_artifact_env(artifact_dirs):
+    environ["ARTIFACT_DIRS"] = ",".join(artifact_dirs)
+    yield
+    environ["ARTIFACT_DIRS"] = ""
+
+
+def test_register_model(mlflow_server):
+    """
+    Test registering a model to MLflow's Model Registry.
+    """
+    model_name = "TestRegisteredModel"
+
+    # define a simple model (for example purposes, using a dummy model)
+    class DummyModel(Model):
+        pass
+
+    # configure the code directories
+    artifact_dirs = ["src1", "src2"]
+    with temporary_dirs(artifact_dirs), temporary_artifact_env(artifact_dirs):
+        # Register the model
+        register(DummyModel(), model_name=model_name, mlflow_uri=mlflow_server)
+
+    client = MlflowClient()
+
+    # assert that the model is registered
+    registered_model = client.get_registered_model(model_name)
+    assert registered_model.name == model_name
+
+    # assert that the model has the correct code paths
+    model_versions = client.get_latest_versions(model_name)
+    artifact_path = client.download_artifacts(model_versions[0].run_id, "")
+    for dir in artifact_dirs:
+        assert path.exists(path.join(artifact_path, "artifacts", dir))
+
+    # delete the registered model after the test
+    client.delete_registered_model(model_name)
+
+
+def test_throw_on_model_not_implementing_protocol(mlflow_server):
+    """
+    Test that the model class implements the Model protocol.
+    """
+
+    class WrongModel:
+        pass
+
+    class RightModel(Model):
+        def load_model(self):
+            pass
+
+        def infer(self, input):
+            pass
+
+    with pytest.raises(TypeError):
+        register(WrongModel())
+
+    register(RightModel())
+
+
+def test_model_load_artifact_code(mlflow_server):
+    """
+    Test loading the artifact code paths.
+    """
+    model_name = "TestRegisteredModel"
+
+    class DummyModel(Model):
+        def load_model(self):
+            # Try to read the contents of the artifact file
+            with open("./fixtures/bar.txt", "r") as f:
+                self.loaded = f.read()
+
+        def infer(self, input):
+            # Try to import a function from the artifact code
+            from fixtures.poo import echo
+
+            return echo(self.loaded + input)
+
+    # Switch cwd to the current file directory to register the fixture artifact
+    with temporary_cwd("test"):
+        register(DummyModel(), model_name=model_name, artifact_dirs="fixtures")
+
+    # Create temp dir and switch to it to test the model.
+    # So artifact symlink not to coliide with the current directory
+    with temporary_dirs(["infer"]), temporary_cwd("infer"):
+        model = load_model(f"models:/{model_name}/latest")
+        assert model.predict("test") == "bartest"
